9fcba646e991b5370f4aeeba0ecb7c3cadaa976b
Rajveer Kumar <rajveer.kumar.ug21@gmail.com>
2026-08-14 18:27:49 +0530

    DETAILS_OF_INVENTORY_RECD_CFPD0305

--- backend/app/parser/registry.py
+++ backend/app/parser/registry.py
@@ -14,7 +14,7 @@ from app.parser.formats import account_opened_report, account_closed_report
 from app.parser.formats import id_users_logged_no_transactions_lond2482, id_users_terminals_not_logged_lond2482
 from app.parser.formats import listof_npa_accounts_lond2572
 from app.parser.formats import agewise_report_of_sys_susp_gend7053, advicefortd
-from app.parser.formats import loans_sanction_letter
+from app.parser.formats import loans_sanction_letter, details_of_inventory_recd_cfpd0305
 
 REGISTRY = {
     "ACTCLS-01": account_closed_report.parse,
@@ -71,4 +71,5 @@ REGISTRY = {
     "GL7053-01": agewise_report_of_sys_susp_gend7053.parse,
     "ADVICEFORTD": advicefortd.parse,
     "LOANS_SANCTION_LETTER": loans_sanction_letter.parse,
+    "SY0305-01": details_of_inventory_recd_cfpd0305.parse,
 }
